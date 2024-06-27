6fd12545546062421048ee6d6cf0c55d288b9a5f
hamza <hamza.maksumic@protonmail.com>
2024-06-27 20:03:17 +0200

    fix: remove unnecessary additions to __LOG__.txt

--- flask-server/__autogit__.py
+++ flask-server/__autogit__.py
@@ -3,7 +3,6 @@ from git import Repo
 
 repo_dir = r'C:\Users\admin\fullstack-project\fullstack-project'
 commit_message = f"chore: automatic JSON data sync {datetime.datetime.now().strftime('%d-%m-%Y')}"
-#commit_message = "feat(automation): implement automatic updates via external API for JSON data sync"
 
 def git_push():
     try:
@@ -12,9 +11,6 @@ def git_push():
         repo.index.commit(commit_message)
         origin = repo.remote(name='origin')
         origin.push()
-
-        with open('__LOG__.txt', 'a', encoding='utf-8') as file:
-            file.write(f"{datetime.datetime.now()} - ####UPDATE PUSHED TO REPOSITORY####\n")
 
     except Exception as e:
         with open('__LOG__.txt', 'a', encoding='utf-8') as file:
