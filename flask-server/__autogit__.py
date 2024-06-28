c71fb5569e5f889cbb268f616fde176fc7f43999
hamza <hamza.maksumic@protonmail.com>
2024-06-28 22:11:01 +0200

    fix: add condition that files must be changed to automatically push
    - This is to avoid empty commits
    - Also updated JSON data due to problems with automating fetching

--- flask-server/__autogit__.py
+++ flask-server/__autogit__.py
@@ -7,13 +7,20 @@ commit_message = f"chore: automatic JSON data sync {datetime.datetime.now().strf
 def git_push():
     try:
         repo = Repo(repo_dir)
+        #checking for differences
+        if not repo.is_dirty(untracked_files=True) and not repo.index.diff(None):
+            print("No changes to commit.")
+            return
+        
         repo.git.add(update=True)
         repo.index.commit(commit_message)
         origin = repo.remote(name='origin')
         origin.push()
+        print("Changes pushed successfully.")
 
     except Exception as e:
         with open('__LOG__.txt', 'a', encoding='utf-8') as file:
-            file.write(f"{datetime.datetime.now()} - error: {e}\n")   
+            file.write(f"{datetime.datetime.now()} - error: {e}\n")
+        print(f"Error: {e}")
 
 git_push()
